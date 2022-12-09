9b73c805b01c2a9cc7c1aa1c4140779574895f94
abnish kumar <abnish.kumar@gogroup.tech>
2022-12-09 12:48:32 +0530

    create a job

---
+++ src/common/Sidebar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import '../stylesheets/sidebar.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faDashboard, faGear, faList, faBell } from '@fortawesome/free-solid-svg-icons';
+import Header from '../common/Header';
+
+function Sidebar() {
+	const [expanded, setExpanded] = useState(true);
+
+	const goToDashboard = () => {
+		window.history.pushState({}, '', '/all-jobs');
+		window.location.reload();
+	}
+
+	const goToCreateAJob = () => {
+		window.history.pushState({}, '', '/create-a-job');
+		window.location.reload();
+	}
+
+	const goToAllJob = () => {
+		window.history.pushState({}, '', '/setting');
+		window.location.reload();
+	}
+
+	const goToAllOrders = () => {
+		window.history.pushState({}, '', '/all-orders');
+		window.location.reload();
+	}
+
+	return (
+		<div>
+			<Header />
+			<div className={expanded ? 'main_side_bar' : 'w_50'} id='main_side_bar'>
+				<nav className='navbar navbar-default' role='navigation'>
+					<div className='navbar-header'>
+						<FontAwesomeIcon icon={faBars} className="icon" id='collapse_expand_btn' onClick={() => setExpanded(!expanded)} />
+					</div>
+					<div className='side-menu-container'>
+						<ul className='nav navbar-nav'>
+							<li className="menu-item" onClick={goToDashboard}><FontAwesomeIcon icon={faDashboard} className='icon' />Dashboard</li>
+							<li className="menu-item" onClick={goToAllOrders}><FontAwesomeIcon icon={faList} className='icon' />Aufträge</li>
+							<li className="menu-item" onClick={goToAllOrders}><FontAwesomeIcon icon={faGear} className='icon' /> Einstellungen</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+	)
+}
+
+export default Sidebar;
