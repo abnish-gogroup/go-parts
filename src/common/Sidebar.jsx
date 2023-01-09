773bcd61b708d1853b8660a25541381592e0de45
abnish kumar <abnish.kumar@gogroup.tech>
2023-01-09 16:43:32 +0530

    sidebar menu

--- src/common/Sidebar.jsx
+++ src/common/Sidebar.jsx
@@ -7,9 +7,16 @@ import Header from '../common/Header';
 
 function Sidebar(props) {
 	const { expanded, handleToggleSidebar } = props;
+	const [showSubMenu, setShowSubMenu] = useState(false);
 
+	console.log('location', window.location.pathname)
 	const goToDashboard = () => {
 		window.history.pushState({}, '', '/dashboard');
+		window.location.reload();
+	}
+
+	const goToInsuranceDashboard = () => {
+		window.history.pushState({}, '', '/insurance-dashboard');
 		window.location.reload();
 	}
 
@@ -23,6 +30,10 @@ function Sidebar(props) {
 		window.location.reload();
 	}
 
+	const handleShowSubmenu =()=>{
+		setShowSubMenu(!showSubMenu);
+	}
+
 	return (
 		<div>
 			<Header expanded={expanded} /> 
@@ -32,11 +43,24 @@ function Sidebar(props) {
 						<FontAwesomeIcon icon={faBars} className="icon" id='collapse_expand_btn' onClick={handleToggleSidebar} />
 					</div>
 					<div className='side-menu-container'>
+						{ window.location.pathname.includes('insurance') ?  
+							<ul className='nav navbar-nav'>
+							<li className="menu-item" onClick={goToInsuranceDashboard}><FontAwesomeIcon icon={faDashboard} className='icon' />dashboard</li>
+							<li className="menu-item" onClick={handleShowSubmenu}><img src='http://static-assets-goparts.s3.amazonaws.com/static-images/control_tool.png' className='icon tool' /> Steuerungs tool</li>
+							{ showSubMenu ?	<ul>
+									<li className="menu-item pdl_15">Regeln managen</li>
+									<li className="menu-item pdl_15">Neue Regel definieren</li>
+									<li className="menu-item pdl_15">Regelwerk</li>
+									<li className="menu-item pdl_15">Analytics</li>
+									<li className="menu-item pdl_15">Import / Export</li>
+								</ul> : '' }
+							<li className="menu-item"><FontAwesomeIcon icon={faGear} className='icon' />Einstellungen</li>
+						</ul> :
 						<ul className='nav navbar-nav'>
 							<li className="menu-item" onClick={goToDashboard}><FontAwesomeIcon icon={faDashboard} className='icon' />Dashboard</li>
 							<li className="menu-item" onClick={goToCreateJob}><FontAwesomeIcon icon={faList} className='icon' />Aufträge</li>
 							<li className="menu-item" onClick={goToAllOrders}><FontAwesomeIcon icon={faGear} className='icon' />Einstellungen</li>
-						</ul>
+						</ul>}
 					</div>
 				</nav>
 			</div>
